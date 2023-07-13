200c139b3c4167db6b714cede5b7b26363a57770
unknown <o.adrian.rodrigues@gmail.com>
2023-07-13 15:23:09 -0300

    recovery data with JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,9 +11,35 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		//Conectar com o banco
+		Connection conn = null;
+		//Executar um comando no Mysql
+		Statement st = null;
+		//guarda o resultado da execução do Statement
+		ResultSet rs = null;
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		try {
+			
+			
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			//escrever aqui abaixo o codigo do MySQL que deseja acessar
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+			
+			
+			} catch(SQLException e) {
+				e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
