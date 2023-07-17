c9f694b94bdeaed28bbe5578e88dd1921ffe7daf
unknown <o.adrian.rodrigues@gmail.com>
2023-07-17 20:48:03 -0300

    DELETE with JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,46 +2,38 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DBIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		Connection conn = null;
 		PreparedStatement st = null;
-
 		try {
-
+			
 			conn = DB.getConnection();
 
 			st = conn.prepareStatement(
-					"UPDATE Seller " + "SET BaseSalary = BaseSalary + ? " + "WHERE " + "(DepartmentId = ?)");
-
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
-
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?");
+			st.setInt(1, 2);
+			
 			int rowsAffected = st.executeUpdate();
-
-			System.out.println("Done! Rows affected: " + rowsAffected);
-
-		} catch (SQLException e) {
-
-			e.printStackTrace();
-
-		} finally {
-			DB.closeConnection();
+			
+			System.out.println("Done! rows Affected: " + rowsAffected);
+			
+		}
+		catch (SQLException e) {
+			throw new DBIntegrityException(e.getMessage());
+		}
+		finally {
 			DB.closeStatement(st);
-
+			DB.closeConnection();
 		}
 	}
-
 }
