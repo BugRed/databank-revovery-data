ba7b9293806e80a6bd0c21da585d69f5d4ec2b36
unknown <o.adrian.rodrigues@gmail.com>
2023-07-17 21:14:32 -0300

    Transactions with JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,39 +1,52 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DBIntegrityException;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		try {
-			
-			conn = DB.getConnection();
 
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
-			st.setInt(1, 2);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! rows Affected: " + rowsAffected);
-			
+			conn = DB.getConnection();
+			// parar commits automaticos
+			conn.setAutoCommit(false);
+
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate("UPDATE seller SET " 
+										+ "BaseSalary = 2090 " 
+										+ "WHERE DepartmentId = 1");
+
+			int rows2 = st.executeUpdate("UPDATE seller SET " 
+										+ "BaseSalary = 3090 " 
+										+ "WHERE DepartmentId = 2");
+
+			// confirmar commits automaticos
+			conn.commit();
+
+			System.out.println("rows 1: " + rows1);
+			System.out.println("rows 2: " + rows2);
+
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Cause by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Cause by: " + e1.getMessage());
 		}
-		catch (SQLException e) {
-			throw new DBIntegrityException(e.getMessage());
-		}
+
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
+			}
 		}
 	}
 }
